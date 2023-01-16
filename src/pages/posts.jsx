925d94b1e00329070a92af9b7ee1e2dcfa8a4a84
IOTrue <kjoonc.dev@gmail.com>
2023-01-16 11:27:10 +0900

    메인목록 작업 중

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -1,20 +1,137 @@
 import React,{useState, useEffect}from 'react'
 import styled from 'styled-components'
 import { useDispatch, useSelector} from 'react-redux';
+import { Link } from 'react-router-dom';
+
 //component, hooks
 
+//style, etc
+import '../css/reset.css'
 import '../css/style.css'
+import {COLORS} from '../style/StyleGlobal'
+
+
+
 
 const Posts = () => {
   return (
-    <StPostsWrap>Posts</StPostsWrap>
+    <StPostsWrap>
+      <StPostsUl>
+      <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+        <StPostsCard>
+          <Link to="/" className="postsCardLink">
+            <StPostsCardImg/>
+            <StPostsInfoBox>
+              <StPostsAuth>출제자 : 가나다라마바사아자차</StPostsAuth>
+              <StPostsDate>출제일 : 2023-01-01</StPostsDate>
+              <StPostsDifficulty>난이도 : ⭐⭐⭐⭐⭐</StPostsDifficulty>
+            </StPostsInfoBox>
+          </Link>
+        </StPostsCard>
+      </StPostsUl>
+    </StPostsWrap>
   )
 }
 
 
+const StPostsDifficulty=styled.span`
+  
+`
+const StPostsDate=styled.span`
+  margin-bottom: 10px;
+`
+const StPostsAuth=styled.span`
+  margin-bottom: 10px;
+`
+const StPostsInfoBox=styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  width: calc(100% - 40px);
+  padding: 0 20px;
+`
+const StPostsCardImg=styled.img.attrs({
+  src: `img/myGiraffe.png`
+})`
+  width: 80%;
+  border: 1px solid ${COLORS.defaultLight};
+  border-radius: 20px;
+  margin-bottom: 10px;
+`
+
+const StPostsCard=styled.li`
+  border: 1px solid ${COLORS.defaultLight};
+  border-radius: 20px;
+  width: 23.6%;
+`
+const StPostsUl=styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px;
+`
 const StPostsWrap=styled.div`
-  border: 1px solid red;
-  max-width: 1000px;
+  border: 1px solid ${COLORS.defaultBold};
+  max-width: 1300px;
   min-width: 380px;
 
   margin: 0 auto;
