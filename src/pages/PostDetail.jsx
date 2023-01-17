9c631014a131b51a4883fe0783562039674dd620
olive-jam <120158932+olive-jam@users.noreply.github.com>
2023-01-18 01:35:59 +0900

    로그인 수정중 / 서버연결 성공

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,196 +1,211 @@
-import React, { useState, useEffect, useRef  } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 
 //component, hooks
-import ButtonDefault from '../components/ButtonDefault';
-import InputWithLabelDefault from '../components/InputWithLabelDefault';
-import useInput from '../hooks/useInput';
-import { __getPostDetail } from '../redux/modules/postDetailSlice';
-import PostsDetailSuccessListAuth from '../components/PostsDetailSuccessListAuth';
-import PostsDetailSuccessListBox from '../components/PostsDetailSuccessListBox';
+import ButtonDefault from "../components/ButtonDefault";
+import InputWithLabelDefault from "../components/InputWithLabelDefault";
+import useInput from "../hooks/useInput";
+import { __getPostDetail } from "../redux/modules/postDetailSlice";
+import PostsDetailSuccessListAuth from "../components/PostsDetailSuccessListAuth";
+import PostsDetailSuccessListBox from "../components/PostsDetailSuccessListBox";
 
 //style, etc
 import styled from "styled-components";
-import {COLORS} from '../style/StyleGlobal'
-import '../css/style.css'
-
-
+import { COLORS } from "../style/StyleGlobal";
+import "../css/style.css";
 
 const PostDetail = () => {
+  const dispatch = useDispatch();
+  const param = useParams();
+  const paramId = parseInt(param.postId); //파라메터값 숫자열로 변환
+  const navigate = useNavigate();
 
-  const dispatch=useDispatch()
-  const param = useParams()
-  const paramId = parseInt(param.postId) //파라메터값 숫자열로 변환
-  const navigate=useNavigate()
+  const { isLoading, error, postDetail } = useSelector(
+    (state) => state.postDetailSlice.data,
+  );
 
-  const { isLoading, error, postDetail } = useSelector((state) => state.postDetailSlice);
-  
+  const dateEdit = postDetail.createdAt?.slice(0, 10); //날짜 형식에 맞게 가공
+  const star = "⭐".repeat(postDetail?.difficult); //난이도 수치에 맞게 별 모양 출력
 
-  const dateEdit = postDetail.createdAt?.slice(0, 10) //날짜 형식에 맞게 가공
-  const star = '⭐'.repeat(postDetail?.difficult) //난이도 수치에 맞게 별 모양 출력
-
-  const answerRef = useRef() // 정답입력인풋
+  const answerRef = useRef(); // 정답입력인풋
 
   //answer 상태, 상태 메세지
-  const [isAnswer, setIsAnswer]=useState(false)
-  const [validMessageAnswer, setValidMessageAnswer]=useState('')
+  const [isAnswer, setIsAnswer] = useState(false);
+  const [validMessageAnswer, setValidMessageAnswer] = useState("");
 
   //data 가져오기
-  const [valueAnswer, onChangeValueAnswer, setValuAnswer]=useInput('')
-  const [isHint, setIsHint]=useState(false)
-  
-  const checkPostDetailInputAnswer=(e)=>{ //유효성 검사 닉네임
-    const regexNickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{1,10}$/
-    let {value}=e.target
-    if(!regexNickname.test(value)){
-      setIsAnswer(false)
-      console.log('setIsNickname : ', isAnswer)
-      return setValidMessageAnswer('❗ 한글, 영어, 숫자 / 10자 이내로 입력')
-    } else{
-      setIsAnswer(true)
-      console.log('setIsNickname : ', isAnswer)
-      return setValidMessageAnswer('')
+  const [valueAnswer, onChangeValueAnswer, setValuAnswer] = useInput("");
+  const [isHint, setIsHint] = useState(false);
+
+  const checkPostDetailInputAnswer = (e) => {
+    //유효성 검사 닉네임
+    const regexNickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{1,10}$/;
+    let { value } = e.target;
+    if (!regexNickname.test(value)) {
+      setIsAnswer(false);
+      console.log("setIsNickname : ", isAnswer);
+      return setValidMessageAnswer("❗ 한글, 영어, 숫자 / 10자 이내로 입력");
+    } else {
+      setIsAnswer(true);
+      console.log("setIsNickname : ", isAnswer);
+      return setValidMessageAnswer("");
     }
-  }
+  };
 
+  useEffect(() => {
+    dispatch(__getPostDetail(paramId));
+  }, [dispatch]);
 
-  useEffect(()=>{
-    dispatch(__getPostDetail(paramId))
-  },[dispatch])
-
-  const onSubmitPostsDetailAnswer=(e)=>{
-    e.preventDefault()
-    if(!isAnswer){
-      return answerRef.current.focus()
+  const onSubmitPostsDetailAnswer = (e) => {
+    e.preventDefault();
+    if (!isAnswer) {
+      return answerRef.current.focus();
     }
-  }
+  };
 
-  const onClickViewHint=()=>{
-    setIsHint(true)
-  }
+  const onClickViewHint = () => {
+    setIsHint(true);
+  };
 
-  const onClickNavigatePosts = ()=>{ //메인으로 가기
-    navigate('/posts')
-  }
-
+  const onClickNavigatePosts = () => {
+    //메인으로 가기
+    navigate("/posts");
+  };
 
   return (
     <StPostsWrap>
       <StPostsBox>
-        <StPostsDetailImg/>
+        <StPostsDetailImg />
         <StPostsDetailInfoBox>
           <StPostsDetailInfoWrite>
-            <StPostsDetailInfoWriteAuth>출제자 : {postDetail?.nickname}</StPostsDetailInfoWriteAuth>
-            <StPostsDetailInfoWriteDate>출제일 : {dateEdit}</StPostsDetailInfoWriteDate>
-            <StPostsDetailInfoWriteDifficulty>난이도 : {star}</StPostsDetailInfoWriteDifficulty>
+            <StPostsDetailInfoWriteAuth>
+              출제자 : {postDetail?.nickname}
+            </StPostsDetailInfoWriteAuth>
+            <StPostsDetailInfoWriteDate>
+              출제일 : {dateEdit}
+            </StPostsDetailInfoWriteDate>
+            <StPostsDetailInfoWriteDifficulty>
+              난이도 : {star}
+            </StPostsDetailInfoWriteDifficulty>
           </StPostsDetailInfoWrite>
-          {
-            !postDetail?.inputAnswer
-            ? (
-                <StPostsDetailInfoAnswer>
-                  {!isHint
-                  ? <ButtonDefault onClick={onClickViewHint} bgColor={COLORS.defaultLight} hoverBgColor={COLORS.defaultBold}>
-                  힌트 보기</ButtonDefault>
-                  : <StHintSpan>힌트 : {postDetail?.inputHint}</StHintSpan>
-                  }
-                  <StPostsDetailInfoAnswerForm onSubmit={onSubmitPostsDetailAnswer}>
-                    <InputWithLabelDefault inputRef={answerRef} autoFocus="autofocus" inputType="text" inputId="postsDetailInputAnswer"
-                    inputValue={valueAnswer} inputPaceholder="정답 입력하기"
-                    width="90%" onChange={onChangeValueAnswer}  validMessage={validMessageAnswer}
-                    onBlur={checkPostDetailInputAnswer} className="postDetailInputAnswer"
-                    />
-                    <ButtonDefault bgColor={COLORS.defaultOrangeLight} hoverBgColor={COLORS.defaultOrange}>정답 제출</ButtonDefault>
-                  </StPostsDetailInfoAnswerForm>
-                </StPostsDetailInfoAnswer>
-              )
-            : (
-              <StPostsDetailInfoAnswer>
-                  <StHintSpan>힌트 : {postDetail?.inputHint}</StHintSpan>
-                  <StAnswerSpan>정답 : {postDetail?.inputHint}</StAnswerSpan>
-                </StPostsDetailInfoAnswer>
-            )
-          }
-          
+          {!postDetail?.inputAnswer ? (
+            <StPostsDetailInfoAnswer>
+              {!isHint ? (
+                <ButtonDefault
+                  onClick={onClickViewHint}
+                  bgColor={COLORS.defaultLight}
+                  hoverBgColor={COLORS.defaultBold}
+                >
+                  힌트 보기
+                </ButtonDefault>
+              ) : (
+                <StHintSpan>힌트 : {postDetail?.inputHint}</StHintSpan>
+              )}
+              <StPostsDetailInfoAnswerForm onSubmit={onSubmitPostsDetailAnswer}>
+                <InputWithLabelDefault
+                  inputRef={answerRef}
+                  autoFocus="autofocus"
+                  inputType="text"
+                  inputId="postsDetailInputAnswer"
+                  inputValue={valueAnswer}
+                  inputPaceholder="정답 입력하기"
+                  width="90%"
+                  onChange={onChangeValueAnswer}
+                  validMessage={validMessageAnswer}
+                  onBlur={checkPostDetailInputAnswer}
+                  className="postDetailInputAnswer"
+                />
+                <ButtonDefault
+                  bgColor={COLORS.defaultOrangeLight}
+                  hoverBgColor={COLORS.defaultOrange}
+                >
+                  정답 제출
+                </ButtonDefault>
+              </StPostsDetailInfoAnswerForm>
+            </StPostsDetailInfoAnswer>
+          ) : (
+            <StPostsDetailInfoAnswer>
+              <StHintSpan>힌트 : {postDetail?.inputHint}</StHintSpan>
+              <StAnswerSpan>정답 : {postDetail?.inputHint}</StAnswerSpan>
+            </StPostsDetailInfoAnswer>
+          )}
+
           {/* 정답자 컴포넌트 */}
-          <PostsDetailSuccessListBox/>
+          <PostsDetailSuccessListBox />
         </StPostsDetailInfoBox>
-        <BsFillArrowLeftCircleFill onClick={onClickNavigatePosts}
-        className="iconArrow prevIcon" aria-label="메인 페이지로 이동"/>
+        <BsFillArrowLeftCircleFill
+          onClick={onClickNavigatePosts}
+          className="iconArrow prevIcon"
+          aria-label="메인 페이지로 이동"
+        />
       </StPostsBox>
     </StPostsWrap>
   );
 };
 
-
-const StHintSpan=styled.span`
+const StHintSpan = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 40px;
   font-weight: bold;
-`
-const StAnswerSpan=styled.span`
+`;
+const StAnswerSpan = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 40px;
   font-weight: bold;
-  color: ${COLORS.defaultOrange}
-`
-const StPostsDetailInfoAnswerForm=styled.form`
+  color: ${COLORS.defaultOrange};
+`;
+const StPostsDetailInfoAnswerForm = styled.form`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   margin-top: 5px;
-`
-const StPostsDetailInfoAnswer=styled.div`
+`;
+const StPostsDetailInfoAnswer = styled.div`
   margin-top: 25px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   row-gap: 10px;
-`
-const StPostsDetailInfoWriteDifficulty=styled.span`
-  
-`
-const StPostsDetailInfoWriteDate=styled.span`
-  
-`
-const StPostsDetailInfoWriteAuth=styled.span`
-
-`
-const StPostsDetailInfoWrite=styled.div`
+`;
+const StPostsDetailInfoWriteDifficulty = styled.span``;
+const StPostsDetailInfoWriteDate = styled.span``;
+const StPostsDetailInfoWriteAuth = styled.span``;
+const StPostsDetailInfoWrite = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 10px;
-`
+`;
 
-const StPostsDetailInfoBox=styled.div`
+const StPostsDetailInfoBox = styled.div`
   width: 50%;
-`
-const StPostsDetailImg=styled.img.attrs(props=>({
-  src: `${props.src || '/img/myGiraffe.png'}`,
-  alt: `${props.alt || '썸네일 이미지'}`,
+`;
+const StPostsDetailImg = styled.img.attrs((props) => ({
+  src: `${props.src || "/img/myGiraffe.png"}`,
+  alt: `${props.alt || "썸네일 이미지"}`,
 }))`
   width: 400px;
   border: 1px solid ${COLORS.defaultBold};
   border-radius: 30px;
   margin-right: 20px;
-`
+`;
 
-const StPostsBox=styled.div`
+const StPostsBox = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: row;
   width: 720px;
   height: 490px;
   position: relative;
-`
+`;
 const StPostsWrap = styled.div`
   background-image: url("/img/myGiraffe_BG-01.png"),
     url("/img/myGiraffe_BG-02.png");
--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -1,22 +1,55 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+axios.defaults.withCredentials = true;
 // Initial State
 const initialState = {
   users: [],
   isLoading: false,
   error: null,
 };
+// //GET
+// export const __getUsers = createAsyncThunk(
+//   "getUsers",
+//   async (payload, thunkAPI) => {
+//     //console.log("payload=", payload);
+//     try {
+//       const { data } = await axios.get(
+//         //"http://localhost:3001/users/",
+//         // "http://prachang.shop/api/users/",
+//         payload,
+//       );
+//       // console.log("payload=", payload);
+//       // console.log("data=", data);
+//       return thunkAPI.fulfillWithValue(data);
+//     } catch (error) {
+//       //console.log(error);
+//       return thunkAPI.rejectWithValue(error);
+//     }
+//   },
+// );
 
+//POST
 export const __postUsers = createAsyncThunk(
   "postUsers",
   async (payload, thunkAPI) => {
     //console.log("payload=", payload);
     try {
-      const { data } = await axios.post(
-        "http://localhost:3001/users/",
-        //"http://prachang.shop/api/users/",
-        payload,
-      );
+      const { data } = await axios
+        .post(
+          // "http://localhost:3001/users/",
+          "https://codingtestrg.shop:3001/api/user/login",
+          payload,
+
+          //{ withCredentials: true },
+        )
+        .then((res) => {
+          console.log(res);
+          return res;
+        });
+      // .then(function (response) {
+      //   console.log(response);
+      // });
       console.log("payload=", payload);
       console.log("data=", data);
       return thunkAPI.fulfillWithValue(data);
@@ -32,6 +65,18 @@ const loginSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
+    // [__getUsers.pending]: (state) => {
+    //   state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    // },
+    // [__getUsers.fulfilled]: (state, action) => {
+    //   state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
+    //   state.Users = action.payload; // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+    // },
+    // [__getUsers.rejected]: (state, action) => {
+    //   state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+    //   state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    // },
+    //post
     [__postUsers.pending]: (state) => {
       state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
     },
