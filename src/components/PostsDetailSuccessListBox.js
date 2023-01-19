1b21d69e7785f4b8c56af9c196638471107190cb
IOTrue <kjoonc.dev@gmail.com>
2023-01-19 17:22:45 +0900

    글 상세 정답자 보기 테스트 중

--- src/components/PostsDetailSuccessListBox.js
+++ src/components/PostsDetailSuccessListBox.js
@@ -18,7 +18,7 @@ const PostsDetailSuccessListBox = () => {
     let keyI = 1
     const { isLoading, error, postDetail } = useSelector((state) => state.postDetailSlice);
     const passedPeople = postDetail.passedPeople
-    console.log('정답자 확인 postDetail : ', postDetail)
+    console.log('정답자 명단 보기 : ', passedPeople)
 
   return (
     <StPostsDetailSuccessListBox>
@@ -66,19 +66,19 @@ const StPostsDetailSuccessListUlBox=styled.div`
 `
 const StPostsDetailSuccessListUl=styled.ul`
   width: 100%;
-  height: 194px;
+  height: 150px;
   overflow-y: auto;
-    ::-webkit-scrollbar {
+  ::-webkit-scrollbar {
     width: 10px;
-}
-    ::-webkit-scrollbar-track {
-        background-color: #fa9370;
-        border-radius: 10px;
-    }
-    ::-webkit-scrollbar-thumb {
-        background-color: #f44408;
-        border-radius: 10px;
-    }
+  }
+  ::-webkit-scrollbar-track {
+      background-color: #fa9370;
+      border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+      background-color: #f44408;
+      border-radius: 10px;
+  }
 
 `
 const StPostsDetailSuccessListBoxTitle=styled.span`
